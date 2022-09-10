fa47bab8f2c3a10e87b97aaee247d59f4854ba92
Mehmet Ulga <mehmetulga@gmail.com>
2022-09-10 14:23:12 +0200

    new portfolio added

---
+++ main.js
@@ -0,0 +1,7 @@
+const mehmetUlga = [
+  {
+    job: "Frontend Developer",
+    skills: "html, css, javascript, PHP, Vue.js, Bootstrap, Wordpress",
+    language: "türkçe, english, deutsch",
+  },
+];
