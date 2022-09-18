d896349247e645865c8a6aab6f25e0b8948914bc
Mehmet Ulga <mehmetulga@gmail.com>
2022-09-18 15:16:18 +0200

    email

--- main.js
+++
@@ -1,7 +0,0 @@
-const mehmetUlga = [
-  {
-    job: "Frontend Developer",
-    skills: "html, css, javascript, PHP, Vue.js, Bootstrap, Wordpress",
-    language: "türkçe, english, deutsch",
-  },
-];
