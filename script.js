144eb20b4ea67f57ad5da020f239f45f781a6b9b
Mehmet Ulga <mehmetulga@gmail.com>
2022-09-18 15:15:21 +0200

    emial

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-// const contactForm = document.querySelector(".contact-form");
-function sendMail(e) {
-  e.preventDefault();
+const form = document.querySelector(".contact-form");
 
-  const contactForm = document.querySelector("#contactForm");
-  const inputName = document.querySelector("#inputName");
-  const inputEmail = document.querySelector("#inputEmail");
-  const message = document.querySelector("#formText");
+function sendMsg(e) {
+  e.preventDefault();
+  const name = document.querySelector(".input-name");
+  const email = document.querySelector(".input-email");
+  const msg = document.querySelector(".message");
+
+  //function
 
   Email.send({
-    SecureToken: "386f7d60-67c4-4633-b18f-59d7ca80756b ",
+    SecureToken: "69334d78-fec0-48d4-b323-9c52fb4ee95c",
     To: "mehmetulga@gmail.com",
-    From: inputEmail.value,
+    From: "mehmetulga@gmail.com",
     Subject: "Contact Form",
-    Body: inputName.value + message.value,
+    Body: name.value + " " + email.value + " " + msg.value,
   }).then((message) => alert(message));
 }
 
-// password 8852D3A3E43F10DA00DB9196AEBF13C87755
-
-// guvenlik token 1cba0eb4-d640-4f3b-aa71-dbbef0101082
-
-// gmail password 1A60BC21F04E45DD1A05F01EBA7438F6A2F4
-// gmail secure token 386f7d60-67c4-4633-b18f-59d7ca80756b
+form.addEventListener("submit", sendMsg);
